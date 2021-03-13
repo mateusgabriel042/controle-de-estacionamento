42d4a9cedf3db2130864a110c46995f150dec6cd
Mateus Gabriel <mateus.gabriel042@gmail.com>
2021-03-13 01:30:23 -0300

    Resolvendo erro no arquivo UserController.php

--- app/Http/Controllers/api/UsersController.php
+++ app/Http/Controllers/api/UsersController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
-use App\Models\api\User;
+use App\Models\User;
 
 class UsersController extends Controller {
 
